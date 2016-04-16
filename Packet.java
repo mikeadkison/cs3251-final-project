78e6b3bd1be0b4d26b32f9033cc9f04517db14e7
Mike <mikeadkison@gmail.com>
2016-04-16 17:28:29 -0400

    saving work

--- Packet.java
+++ Packet.java
@@ -215,7 +215,8 @@ public class Packet {
         }
 
         msgDigest.update(messageBytes);
-        return msgDigest.digest();
+        byte[] checksum = msgDigest.digest();
+        return checksum;
     }
 
     @Override
